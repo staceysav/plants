419914bdb7690c98341fd06fc4702bcbf3dcc205
staceysav <nastasia.savinova@gmail.com>
2023-01-17 20:33:17 +0300

    feat: add how to close adaptive menu

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.nav__list ');
-    const close = document.querySelector('.nav__close');
+    const closeItem = document.querySelector('.nav__close');
     burgerItem.addEventListener('click', () => {
         menu.classList.replace('nav__list_inactive', 'nav__list_active')
-        close.classList.replace('nav__close_inactive', 'nav__close_active')
-
+        closeItem.classList.replace('nav__close_inactive', 'nav__close_active')
+    })
+    closeItem.addEventListener(('click'), () => {
+        menu.classList.replace('nav__list_active', 'nav__list_inactive');
+        closeItem.classList.replace('nav__close_active', 'nav__close_inactive');
     })
 }());
