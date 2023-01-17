d99c422984cb63ee67cc3824f029e72b7aaf0cf6
staceysav <nastasia.savinova@gmail.com>
2023-01-17 21:04:43 +0300

    feat: add closing adaptive menu when clicking menu items

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.nav__list ');
     const closeItem = document.querySelector('.nav__close');
+    const menuLinks = document.querySelectorAll('.nav__list-item');
     burgerItem.addEventListener('click', () => {
         menu.classList.replace('nav__list_inactive', 'nav__list_active')
         closeItem.classList.replace('nav__close_inactive', 'nav__close_active')
@@ -10,4 +11,14 @@
         menu.classList.replace('nav__list_active', 'nav__list_inactive');
         closeItem.classList.replace('nav__close_active', 'nav__close_inactive');
     })
+
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 585) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener(('click'), () => {
+                menu.classList.replace('nav__list_active', 'nav__list_inactive');
+                closeItem.classList.replace('nav__close_active', 'nav__close_inactive');
+            })
+        }
+    }
 }());
