9514b8fc9e9d2bd0a96db95104c27762b877d6a3
staceysav <nastasia.savinova@gmail.com>
2023-02-01 15:59:08 +0300

    feat: add accordion

--- main.js
+++ main.js
@@ -96,3 +96,25 @@ function toggleActive(event) {
     event.srcElement.classList.toggle('active-service-button');
     applyBlur();
 }
+
+
+// Adding accordion
+var acc = document.getElementsByClassName("prices-tariffs__button");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active-tariff");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
